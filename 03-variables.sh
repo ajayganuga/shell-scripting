a28c5922c3091d61a5b6603ea509a74f600cac7d
ajayganuga <ajayganuga@gmail.com>
2026-05-31 22:34:18 +0530

    Variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ajay # there is no space before and after equal
+PERSON2=Bro
+
+echo "$PERSON1:: Hi ${PERSON2}" 
+echo "${PERSON2}:: Hello $PERSON1"
+echo "$PERSON1:: How are you doing?"
+echo "$PERSON2:: I am good. How are you?"
