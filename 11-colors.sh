11a0803b3896f2d10cf9a4c993a6aaae9ec47722
ajayganuga <ajayganuga@gmail.com>
2026-06-02 22:19:47 +0530

    Colors

---
+++ 11-colors.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo -e "\e[1;32m[SUCCESS]\e[0m Application Started"
+
+echo -e "\e[1;33m[WARNING]\e[0m Low Memory"
+
+echo -e "\e[1;31m[ERROR]\e[0m Database Connection Failed"
+
+echo -e "\e[1;34m[INFO]\e[0m Server Running"
